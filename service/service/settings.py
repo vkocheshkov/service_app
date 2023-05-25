2f1e26c0f558283867c635c5f695cec9b488ca9c
vkocheshkov <kocheshkov.v@yandex.ru>
2023-05-25 11:38:38 +0300

    Added SQL logging in Django settings file && added prefetch related and select related

--- service/service/settings.py
+++ service/service/settings.py
@@ -137,3 +137,25 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
